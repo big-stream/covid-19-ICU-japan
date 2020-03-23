5b69c8c770cfe3a78344a772fe7576ceec34de94
big-stream <37651140+big-stream@users.noreply.github.com>
2020-03-23 20:56:46 +0900

    -

--- make-js.sh
+++ make-js.sh
@@ -4,9 +4,13 @@ echo 'var db = [' > db.js
 
 sed -r -n "
 2,$ {
-  s/^([0-9]+-[0-9]+-[0-9]+),([0-9]+,)([0-9]+,)([0-9]+,)(.+)/  [new Date('\1 12:00'), \2 \3 \4],/g p
+  s/^([0-9]+-[0-9]+-[0-9]+),([0-9]+,)([0-9]+,)([0-9]+,)(.+)/  [new Date('\1'), \2 \3 \4],/g p
 }
 " db.csv >> db.js
 
 echo ']' >> db.js
+
+# 最新URL
+url=$( tail -2 db.csv | grep '^202' | tail -1 | cut -f 5 -d,)
+echo "var url = '$url'" >> db.js
 
