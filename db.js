23198279231dc1c78e7c760a76ce1b200714fb1f
big-stream <37651140+big-stream@users.noreply.github.com>
2020-04-07 21:59:02 +0900

    データ更新

--- db.js
+++ db.js
@@ -36,6 +36,7 @@ var db = [
   [new Date('2020-04-04'), 69, 69,],
   [new Date('2020-04-05'), 70, 70,],
   [new Date('2020-04-06'), 79, 73,],
+  [new Date('2020-04-07'), 80, 80,],
 ]
 var ventilator = [
   [0, "全国", 15415, 75451, 35578, 126444, 18322],
@@ -92,4 +93,4 @@ ventilator.forEach(e => {
   e.push(Math.round(e[6]/e[4]*100)/10)
   e.push(Math.round(e[6]/(e[3]+e[4])*100)/10)
 })
-var url = 'https://www.mhlw.go.jp/stf/newpage_10709.html'
+var url = 'https://www.mhlw.go.jp/stf/newpage_10723.html'
